abbd2ab784d0a9b3da11a5262c0d3555803e172c
Nihal Deb <nihaldeb2006@gmail.com>
2024-06-23 16:45:19 +0800

    Update app.js with rate limiter

--- src/app.js
+++ src/app.js
@@ -16,18 +16,29 @@ const mainRoute = require('./routes/mainRoutes');
 // CREATE APP
 //////////////////////////////////////////////////////
 const app = express();
-
+// add rate limiter to prevent dos attack
+const RateLimit = require('express-rate-limit');
+const limiter = RateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // max 100 requests per windowMs
+});
 //////////////////////////////////////////////////////
 // USES
 //////////////////////////////////////////////////////
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
+app.use(limiter);
 
 //////////////////////////////////////////////////////
 // SETUP ROUTES
 //////////////////////////////////////////////////////
 app.use('/api', mainRoute);
 app.use('/', express.static('public'))
+app.get('/:path', function(req, res) {
+  let path = req.params.path;
+  if (isValidPath(path))
+    res.sendFile(path);
+});
 
 //////////////////////////////////////////////////////
 // EXPORT APP
